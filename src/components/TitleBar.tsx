0ab94d864973c2672910558e041b37025fc683cf
David Alexander Bjerremose <dab@davidbjerremose.com>
2025-06-29 19:13:31 +0200

    feat: better titlebar, font

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,13 +1,6 @@
-import { getCurrentWindow } from '@tauri-apps/api/window';
-import { useState, useEffect } from 'react';
-import { Settings, X, Minus, Maximize2, Minimize2 } from 'lucide-react';
+import { Settings } from 'lucide-react';
 import { Button } from './ui/button';
-
-/*
-Fundamentally, having to completely replace the window's title bar is kind of off-putting.
-Ideally, Tauri would have some kind of variant of what e.g. Electron's title bar is.
-But, it is what it is for now.
-*/
+import { platform } from '@tauri-apps/plugin-os';
 
 interface TitleBarProps {
   isSettingsOpen: boolean
@@ -15,18 +8,29 @@ interface TitleBarProps {
 }
 
 export function TitleBar({ setIsSettingsOpen }: TitleBarProps) {
-  const handleMinimize = () => getCurrentWindow().minimize();
-  const handleMaximize = () => getCurrentWindow().toggleMaximize();
-  const handleClose = () => getCurrentWindow().close();
 
+  // Left is not visible on macOS
+  const isLeftVisible = platform() !== 'macos';
+  
   return (
     <div
       data-tauri-drag-region
-      className="fixed top-0 left-0 right-0 h-8 flex items-center justify-end select-none bg-accent"
+      className="fixed top-0 left-0 right-0 h-8 flex items-center justify-between select-none bg-accent"
     >
-      <div className="flex items-center gap-2 pl-4">
+      <div className={`flex items-center gap-2 ${!isLeftVisible ? 'invisible' : ''}`}>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-8 w-12 hover:bg-zinc-200 dark:hover:bg-zinc-800"
+          onClick={() => setIsSettingsOpen(true)}
+        >
+          <Settings className="h-4 w-4" />
+        </Button>
+      </div>
+
+      <div className="flex items-center gap-2">
         <img src="/assets/icon.png" alt="App Icon" className="w-4 h-4" />
-        <span className="text-sm font-medium">CrossZones</span>
+        <span className="text-sm font-medium select-none">CrossZones</span>
       </div>
 
       <div className="flex items-center gap-2">
