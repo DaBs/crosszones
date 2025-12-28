6c872f68b73bd2c052d7e00ec8cf48e4e8374d5b
David Alexander Bjerremose <dab@davidbjerremose.com>
2025-12-28 17:12:43 +0100

    feat: macOS rounded out

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -1,11 +1,17 @@
-import { TabsList, TabsTrigger } from './ui/tabs';
+import { TabsList, TabsTrigger } from '../ui/tabs';
 import { Keyboard, Settings, PanelsTopLeft } from 'lucide-react';
+import { platform } from '@tauri-apps/plugin-os';
+import { useMemo } from 'react';
 
 export function TitleBar() {
+  const isMacOS = useMemo(() => platform() === 'macos', []);
+  
   return (
     <div className="flex w-full justify-center items-center h-12 px-4" data-tauri-drag-region>
       <div className="flex-1" data-tauri-drag-region></div>
-      <TabsList className="inline-flex h-auto rounded-lg bg-muted p-1 shadow-sm">
+      <TabsList 
+        className={`rounded-none rounded-b-md relative inline-flex h-auto bg-muted p-1 shadow-sm ${isMacOS ? 'rounded-out-t-lg' : 'rounded-lg'}`}
+      >
         <TabsTrigger 
           value="hotkeys" 
           className="rounded-md px-4 py-2 text-sm font-medium transition-all data-[state=active]:bg-background data-[state=active]:text-primary data-[state=active]:shadow-sm data-[state=inactive]:text-muted-foreground [&[data-state=active]_svg]:text-primary"
